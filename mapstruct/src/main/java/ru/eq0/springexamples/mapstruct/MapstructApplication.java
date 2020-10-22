6b2130c7a789077a0214ea6e93e96eb2d72552d4
Щербаков Иван Вячеславович_4 <ivan.v.shcherbakov@domru.ru>
2020-10-22 18:03:32 +0800

    add: init mapstruct example

---
+++ mapstruct/src/main/java/ru/eq0/springexamples/mapstruct/MapstructApplication.java
@@ -0,0 +1,13 @@
+package ru.eq0.springexamples.mapstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapstructApplication.class, args);
+	}
+
+}
